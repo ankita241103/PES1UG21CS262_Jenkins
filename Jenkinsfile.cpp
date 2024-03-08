4521e11c211d9519db3778f7cdce79844fefc52a
K Srinivasa Ankita <107118009+ankita241103@users.noreply.github.com>
2024-03-09 00:13:14 +0530

    Update Jenkinsfile.cpp

--- Jenkinsfile.cpp
+++ Jenkinsfile.cpp
@@ -5,7 +5,7 @@ pipeline{
     stage('Build'){
       steps{
         build  'PES1UG21CS262-1'
-        sh 'g++ main.cpp -o output'
+        sh 'g++ Jenkinsfile.cpp -o output'
       }
     }  
       stage('Test'){
